5653b7ef4d725207b0f64929c052b0c1f21b849c
zhouyi <shzy2012@gmail.com>
2024-11-19 11:31:06 +0800

    .

--- src/tools/slice.go
+++ src/tools/slice.go
@@ -8,3 +8,12 @@ func SliceContains(slice []string, str string) bool {
 	}
 	return false
 }
+
+func Reverse[T any](arr []T) []T {
+	// 定义两个索引，一个从开头，一个从末尾开始
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		// 交换元素
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	return arr
+}
